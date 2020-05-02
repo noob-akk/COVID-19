98e2b7dfde6d3cb6374eb8d74d14af699999c31e
Achyuth1 <achyuth6654@gmail.com>
2020-05-02 20:43:00 +0530

    update

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ df = pds.read_csv("India_series.csv")
 delta = np.array(df["Cases"].diff().values, np.int32)
 dates = list(df["Date"].values)
 
-window = 20
+window = 30
+
 plt.figure()
-plt.title("New Cases w.r.t Date")
-plt.plot(list(range(window)), delta[-window:], "b*--")
-plt.xticks(ticks=list(range(window)), labels=[date[5:] for date in dates[-window:]])
+plt.title("New Cases w.r.t Date %d"%(df["Cases"].values[-1]))
+plt.plot(list(range(window)), delta[-window:], "b*-")
+plt.xticks(ticks=list(range(window))[::2], labels=[date[5:] for date in dates[-window::2]])
 plt.annotate("%d cases!"%(delta[-1]), (window-1, delta[-1]), textcoords="offset points", xytext=(0,10), ha='center')
 plt.show()
-plt.close()
